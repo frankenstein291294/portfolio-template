473061cc625063830970a803b194ea0277182f47
frankenstein291294 <frankenstein291294@gmail.com>
2023-03-31 14:18:56 -0600

    First commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+import Typed from '../node_modules/typed.js/dist/typed.module.js';
+let typed = new Typed(".multext", {
+
+  strings: ['coder', "youtuber", 'designer'],
+  loop: true,
+  typeSpeed: 100,
+  backSpeed:80,
+  backDeley: 1500
+
+})
